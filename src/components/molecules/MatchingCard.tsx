d66f13b856b295a30734c87dc58269a9d66484c2
dolphin-pc <pcx474@gmail.com>
2024-07-22 17:50:52 +0900

    [ADD] 메인-운전연수카드

---
+++ src/components/molecules/MatchingCard.tsx
@@ -0,0 +1,25 @@
+import * as S from '@/styles/index.style';
+
+interface Props {
+  imgSrc?: string;
+}
+
+const MatchingCard = (props: Props) => {
+  const { imgSrc = 'https://via.placeholder.com/300x200' } = props; // 기본값으로 JSONPlaceholder
+  return (
+    <S.div.Column $gap={15}>
+      <img src={imgSrc} />
+
+      <S.div.Column $gap={10}>
+        <S.h.h2>운전 메이트 이름</S.h.h2>
+        <S.p.P $maxLines={1}>운전 메이트 소개</S.p.P>
+        <S.div.Row $gap={10}>
+          <S.span.Badge>운전 메이트 나이</S.span.Badge>
+          <S.span.Badge>운전 메이트 성별</S.span.Badge>
+        </S.div.Row>
+      </S.div.Column>
+    </S.div.Column>
+  );
+};
+
+export default MatchingCard;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import * as S from '@/styles/index.style';
 import PostCard from '@/components/organisms/PostCard';
 import Header from '@/components/organisms/Header';
 import ChallengeCard from '@/components/molecules/ChallengeCard';
+import MatchingCard from '@/components/molecules/MatchingCard';
 
 const LandingPage = () => {
   return (
@@ -25,6 +26,12 @@ const LandingPage = () => {
         </S.div.Grid>
 
         <S.h.h1>운전 메이트를 찾아봐요</S.h.h1>
+        <S.div.Row style={{ justifyContent: 'space-between' }}>
+          <MatchingCard />
+          <MatchingCard />
+          <MatchingCard />
+          <MatchingCard />
+        </S.div.Row>
         <S.h.h1>인증된 팁을 공유드려요</S.h.h1>
         <S.h.h1>영상으로 운전을 배워봐요</S.h.h1>
       </S.div.Column>
