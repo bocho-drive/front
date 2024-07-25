457536d83da0d5549b35c1dd35b4e855051fbee2
홍채현 <hyeon9808@gmail.com>
2024-07-25 20:33:33 +0900

    Update AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,18 +1,21 @@
 import SearchBar from '@/components/atoms/SearchBar';
 import Sidebar from '@/components/atoms/Sidebar';
 import PostList from '@/components/organisms/PostList';
+import HeaderFooterLayout from '@/components/templates/HeaderFooterLayout';
 
 import * as S from '@/styles/index.style';
 
 const AdminPage = () => {
   return (
-    <S.div.Container>
-      <Sidebar />
-      <S.div.MainContent>
-        <SearchBar />
-        <PostList />
-      </S.div.MainContent>
-    </S.div.Container>
+    <HeaderFooterLayout>
+      <S.div.Container>
+        <Sidebar />
+        <S.div.MainContent>
+          <SearchBar />
+          <PostList />
+        </S.div.MainContent>
+      </S.div.Container>
+    </HeaderFooterLayout>
   );
 };
 
