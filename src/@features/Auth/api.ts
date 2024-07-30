127178dea00b8f1f5d87d7e36b83e8538dbeb253
dolphin-pc <pcx474@gmail.com>
2024-07-30 16:42:27 +0900

    [Modify] 로그인/회원가입 api url 변경

--- src/@features/Auth/api.ts
+++ src/@features/Auth/api.ts
@@ -2,13 +2,13 @@ import { api, Response } from '@/config/axios';
 import { LoginReq, RegisterReq } from './type';
 
 export const signUp = async (data: RegisterReq): Promise<void> => {
-  const res = await api.post<Response<void>>('api/v1/user/signup', data);
+  const res = await api.post<Response<void>>('signup', data);
 
   return res.data.data;
 };
 
 export const signIn = async (data: LoginReq): Promise<string> => {
-  const res = await api.post<string>('api/v1/user/signin', data);
+  const res = await api.post<string>('signin', data);
 
   return res.data;
 };
