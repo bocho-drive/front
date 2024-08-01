3c72f8f097099a2c57c9ecef43780f95cf9924f0
dolphin-pc <pcx474@gmail.com>
2024-08-01 20:50:37 +0900

    어드민 게시글 목록 조회

--- src/@features/Admin/Post/usePost.ts
+++ src/@features/Admin/Post/usePost.ts
@@ -1,92 +1,99 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+
+// export interface CommunityRes {
+//     id: number;
+//     title: string;
+//     viewCount: number;
+//     verifiedYN: boolean;
+//     createdAt: string;
+//   }
 
 interface Post {
-    id: number;
-    title: string;
-    likes: number;
-    comments: number;
-    isChecked: boolean;
+  // api
+  id: number;
+  title: string;
+  viewCount: number;
+  verifiedYN: boolean;
+  createdAt: string;
+
+  // 클라이언트
+  isChecked: boolean;
+  likes: number;
+  comments: number;
 }
 
 interface Props {
-    posts: Post[];
-    currentPage: number;
-    postsPerPage: number;
-    totalPages: number;
-    allChecked: boolean;
+  posts: Post[];
+  currentPage: number;
+  postsPerPage: number;
+  totalPages: number;
+  allChecked: boolean;
 }
 
 interface Actions {
-    setPosts: (posts: Post[]) => void;
-    handleSort: (criteria: string) => void;
-    handleDelete: () => void;
-    handleToggleAll: (checked: boolean) => void;
-    handleToggle: (id: number) => void;
-    setCurrentPage: (page: number) => void;
+  setPosts: (posts: Post[]) => void;
+  handleSort: (criteria: string) => void;
+  handleDelete: () => void;
+  handleToggleAll: (checked: boolean) => void;
+  handleToggle: (id: number) => void;
+  setCurrentPage: (page: number) => void;
+  setTotalPages: (totalPages: number) => void;
 }
 
 export const usePost = create<Props & Actions>((set, get) => ({
-    posts: [
-        { id: 1, title: 'Post 1', likes: 10, comments: 2, isChecked: false },
-        { id: 2, title: 'Post 2', likes: 5, comments: 4, isChecked: false },
-        { id: 3, title: 'Post 3', likes: 8, comments: 1, isChecked: false },
-        { id: 4, title: 'Post 4', likes: 6, comments: 3, isChecked: false },
-        { id: 5, title: 'Post 5', likes: 4, comments: 5, isChecked: false },
-        { id: 6, title: 'Post 6', likes: 7, comments: 2, isChecked: false },
-        { id: 7, title: 'Post 7', likes: 2, comments: 7, isChecked: false },
-        { id: 8, title: 'Post 8', likes: 3, comments: 4, isChecked: false },
-        { id: 9, title: 'Post 9', likes: 9, comments: 3, isChecked: false },
-        { id: 10, title: 'Post 10', likes: 1, comments: 8, isChecked: false },
-        { id: 11, title: 'Post 11', likes: 5, comments: 6, isChecked: false },
-    ],
-    currentPage: 1,
-    postsPerPage: 10,
-    totalPages: 2,
-    allChecked: false,
+  posts: [],
+  currentPage: 0,
+  postsPerPage: 10,
+  totalPages: 0,
+  allChecked: false,
 
-    setPosts: (posts) => {
-        const postsPerPage = get().postsPerPage;
-        const totalPages = Math.ceil(posts.length / postsPerPage);
-        const allChecked = posts.length > 0 && posts.every(post => post.isChecked);
-        set({ posts, totalPages, allChecked });
-    },
+  setPosts: (posts) => {
+    // const postsPerPage = get().postsPerPage;
+    // const totalPages = Math.ceil(posts.length / postsPerPage);
+    const allChecked = posts.length > 0 && posts.every((post) => post.isChecked);
+    set({ posts, allChecked });
+  },
 
-    handleSort: (criteria) => {
-        const { posts, setPosts } = get();
+  handleSort: (criteria) => {
+    const { posts, setPosts } = get();
 
-        const sortedPosts = [...posts].sort((a, b) => {
-            if (criteria === 'likes') {
-                return b.likes - a.likes;
-            } else if (criteria === 'comments') {
-                return b.comments - a.comments;
-            }
-            return 0;
-        });
-        setPosts(sortedPosts);
-    },
+    const sortedPosts = [...posts].sort((a, b) => {
+      if (criteria === 'likes') {
+        return b.likes - a.likes;
+      } else if (criteria === 'comments') {
+        return b.comments - a.comments;
+      }
+      return 0;
+    });
+    setPosts(sortedPosts);
+  },
 
-    handleDelete: () => {
-        const { posts, setPosts } = get();
-        const remainingPosts = posts.filter((post) => !post.isChecked);
-        setPosts(remainingPosts);
-    },
+  handleDelete: () => {
+    const { posts, setPosts } = get();
+    const remainingPosts = posts.filter((post) => !post.isChecked);
+    setPosts(remainingPosts);
+  },
 
-    handleToggleAll: (checked) => {
-        const { posts, setPosts } = get();
-        const updatedPosts = posts.map((post) => ({ ...post, isChecked: checked }));
-        setPosts(updatedPosts);
-    },
+  handleToggleAll: (checked) => {
+    const { posts, setPosts } = get();
+    const updatedPosts = posts.map((post) => ({ ...post, isChecked: checked }));
+    setPosts(updatedPosts);
+  },
 
-    handleToggle: (id) => {
-        const { posts, setPosts } = get();
-        const updatedPosts = posts.map((post) => (post.id === id ? { ...post, isChecked: !post.isChecked } : post));
-        setPosts(updatedPosts);
-    },
+  handleToggle: (id) => {
+    const { posts, setPosts } = get();
+    const updatedPosts = posts.map((post) => (post.id === id ? { ...post, isChecked: !post.isChecked } : post));
+    setPosts(updatedPosts);
+  },
 
-    setCurrentPage: (page) => {
-        const { totalPages } = get();
-        if (page > 0 && page <= totalPages) {
-            set({ currentPage: page });
-        }
+  setCurrentPage: (page) => {
+    const { totalPages } = get();
+    if (page > 0 && page <= totalPages) {
+      set({ currentPage: page });
     }
+  },
+
+  setTotalPages: (totalPages) => {
+    set({ totalPages });
+  },
 }));
--- src/components/organisms/PostList.tsx
+++ src/components/organisms/PostList.tsx
@@ -2,26 +2,48 @@ import PostListHeader from '../molecules/PostListHeader';
 import * as S from '@/styles/index.style';
 import { usePost } from '@/@features/Admin/Post/usePost';
 import { useNavigate } from 'react-router-dom';
+import { getCommunityList } from '@/@features/Community/api';
+import { useEffect } from 'react';
 
 const PostList = () => {
-  const { posts, currentPage, postsPerPage, totalPages, handleToggle, setCurrentPage } = usePost();
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const { posts, setPosts, currentPage, handleToggle, setCurrentPage, totalPages, setTotalPages } = usePost();
+  const navigate = useNavigate();
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
-  const navigate = useNavigate();
-
   const handleToAdminDetail = (id: number) => navigate(`/admin/detail/${id}`);
+
+  const fetchPosts = async () => {
+    const data = await getCommunityList({
+      category: 'GENERAL',
+      page: currentPage - 1,
+    });
+    if (data) {
+      setTotalPages(data.totalPages);
+      const posts = data.content.map((post) => ({
+        id: post.id,
+        title: post.title,
+        viewCount: post.viewCount,
+        verifiedYN: post.verifiedYN,
+        createdAt: post.createdAt,
+        isChecked: false,
+        likes: 0,
+        comments: 0,
+      }));
+      setPosts(posts);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, [currentPage]);
 
   return (
     <S.div.PostListContainer>
       <PostListHeader />
-      {currentPosts.map((post) => (
+      {posts.map((post) => (
         <S.div.PostItem key={post.id}>
           <div>
             <S.input.Checkbox id={`post-${post.id}`} type="checkbox" checked={post.isChecked} onChange={() => handleToggle(post.id)} />
@@ -29,7 +51,7 @@ const PostList = () => {
               {post.title}
             </label>
           </div>
-          <div>추천: {post.likes}</div>
+          <div>조회: {post.viewCount}</div>
           <div>댓글: {post.comments}</div>
         </S.div.PostItem>
       ))}
