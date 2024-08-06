ff0eb55653434852c04df6dbed26ed781f1a1f48
Chanyoung Park <43159931+Dolphin-PC@users.noreply.github.com>
2024-08-06 11:06:59 +0900

    Feat/매칭신청 (#75)
    * #67 매칭 신청 등록/취소/목록조회
    
    * 매칭 상세 화면, status/type 뱃지 UI추가

--- src/pages/MatchingDetailPage.tsx
+++ src/pages/MatchingDetailPage.tsx
@@ -7,11 +7,19 @@ import Loading from '@/components/atoms/Loading';
 import { getDateString } from '@/util/util';
 import { useAuth } from '@/@features/Auth/useAuth';
 import { Fragment } from 'react/jsx-runtime';
+import ApplyButton from '@/@features/MatchingApply/components/ApplyButton';
+import ApplyList from '@/@features/MatchingApply/components/ApplyList';
+import ErrorSuspenseLayout from '@/components/templates/ErrorSuspenseLayout';
+import { MatchingStatus, MatchingType } from '@/@features/Matching/components/MatchingCard';
 
 const MatchingDetailPage = () => {
   const { id } = useParams();
   const { data, isLoading } = useMatchingQuery(Number(id));
-  const userId = useAuth((state) => state.userId);
+  const { userId, userRole, isAuth } = useAuth((state) => ({
+    userId: state.userId,
+    userRole: state.userRole,
+    isAuth: state.isAuth,
+  }));
   const isAuthor = data?.userId === userId;
 
   const navigate = useNavigate();
@@ -47,6 +55,11 @@ const MatchingDetailPage = () => {
               </S.div.Row>
               <KakaoShareButton title={data.title} />
             </S.div.Row>
+
+            <S.div.Row $gap={10}>
+              <MatchingStatus status={data.status} />
+              <MatchingType type={data.type} />
+            </S.div.Row>
             <S.h.H1>{data.title}</S.h.H1>
 
             <S.div.Row $gap={10} $align="center">
@@ -60,15 +73,23 @@ const MatchingDetailPage = () => {
             <S.span.Span>{data.content}</S.span.Span>
           </S.div.Column>
 
-          <S.div.Row $gap={10} $justify="center">
-            <S.button.Button>매칭 신청하기</S.button.Button>
-          </S.div.Row>
+          {userRole === 'TEACHER' && <ApplyButton matchingId={Number(id)} />}
 
           <S.div.Gap $height={20} />
 
           <S.div.Row $gap={10} $justify="flex-start">
             <S.button.Button onClick={handleToList}>목록으로</S.button.Button>
           </S.div.Row>
+
+          {isAuth && (
+            <Fragment>
+              <S.hr.Hr />
+              <S.h.H4>신청 목록</S.h.H4>
+              <ErrorSuspenseLayout>
+                <ApplyList matchingId={Number(id)} />
+              </ErrorSuspenseLayout>
+            </Fragment>
+          )}
         </S.div.Column>
       )}
     </DriveLayout>
