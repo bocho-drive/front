ddeffa50f52a15794e20ba30cb2dd3ca8b83f0e2
dolphin-pc <pcx474@gmail.com>
2024-08-06 11:34:04 +0900

    커뮤니티 조회
    커뮤니티 조회

--- src/components/molecules/CommunityCard.tsx
+++ src/components/molecules/CommunityCard.tsx
@@ -3,20 +3,24 @@ import * as S from '@/styles/index.style';
 
 interface Props {
   data: CommunityRes;
+  topComponent?: React.ReactNode;
+  bottomComponent?: React.ReactNode;
 }
 
-const CommunityCard = ({ data }: Props) => {
+const CommunityCard = ({ data, topComponent, bottomComponent }: Props) => {
   return (
     <S.div.Card>
       <S.div.Column $gap={20}>
-        <S.h.H5>{data.author}</S.h.H5>
+        {topComponent}
         <S.h.H2>{data.title}</S.h.H2>
+        {/* <S.small.Small>작성자 : {data.author}</S.small.Small> */}
         <S.div.Row $gap={10} $between>
           <S.div.Row $gap={10} $align="center">
-            <S.p.P>조회 {data.viewCount}</S.p.P>
-            <S.p.P>추천 {data.likeCount}</S.p.P>
+            <S.p.P>조회 {data.viewCount.toLocaleString('ko-KR')}</S.p.P>
+            <S.p.P>추천 {data.likeCount.toLocaleString('ko-KR')}</S.p.P>
           </S.div.Row>
         </S.div.Row>
+        {bottomComponent}
       </S.div.Column>
     </S.div.Card>
   );
--- src/components/organisms/CommunityCommentCardList.tsx
+++ src/components/organisms/CommunityCommentCardList.tsx
@@ -1,15 +1,16 @@
 import * as S from '@/styles/index.style';
 import SimpleComment from '../molecules/SimpleComment';
-import LineText from '../atoms/LineText';
 import { Link } from 'react-router-dom';
 import { useSuspenseQueries, useSuspenseQuery } from '@tanstack/react-query';
 import { getCommunityList } from '@/@features/Community/api';
 import { getCommentList } from '@/@features/Comment/api';
+import CommunityCard from '../molecules/CommunityCard';
+import { Fragment } from 'react/jsx-runtime';
 
 const CommunityCommentCardList = () => {
   const { data: communityListlimitTwo } = useSuspenseQuery({
     queryKey: ['communityList'],
-    queryFn: () => getCommunityList({ category: 'GENERAL', page: 0, size: 2, sortBy: 'viewCount', isAsc: false }),
+    queryFn: () => getCommunityList({ category: 'GENERAL', page: 0, size: 2 }),
     retry: 1,
   });
 
@@ -25,28 +26,24 @@ const CommunityCommentCardList = () => {
       {communityListlimitTwo.content.map((community, index) => (
         <Link to={`/community/${community.id}`} key={community.id}>
           <S.div.Column $gap={10}>
-            <S.div.Card>
-              <S.div.Column $gap={20}>
-                <S.span.Badge>조회수 TOP</S.span.Badge>
-                <S.h.H2>{community.title}</S.h.H2>
-
-                <S.div.Row $gap={10}>
-                  <S.div.Row $gap={5} $align="center"></S.div.Row>
-                  <S.div.Row $gap={5} $align="center">
-                    <S.h.H5>조회수 {community.viewCount}</S.h.H5>
-                  </S.div.Row>
-                </S.div.Row>
-
-                <LineText />
-
-                <S.div.Column $gap={20}>
-                  {/* FIXME 댓글 limit추가 필요 */}
-                  {commentList[index].data?.slice(0, 2).map((comment) => (
-                    <SimpleComment key={comment.id} data={comment} />
-                  ))}
-                </S.div.Column>
-              </S.div.Column>
-            </S.div.Card>
+            <S.div.Column $gap={20}>
+              <CommunityCard
+                data={community}
+                topComponent={<S.span.Badge>최신 글</S.span.Badge>}
+                bottomComponent={
+                  commentList[index].data.length > 0 && (
+                    <Fragment>
+                      <S.hr.Hr />
+                      <S.div.Column $gap={20}>
+                        {commentList[index].data?.slice(0, 2).map((comment) => (
+                          <SimpleComment key={comment.id} data={comment} />
+                        ))}
+                      </S.div.Column>
+                    </Fragment>
+                  )
+                }
+              />
+            </S.div.Column>
           </S.div.Column>
         </Link>
       ))}
