b86284e802ce4afe96f011e4f316a7a008c701dc
dolphin-pc <pcx474@gmail.com>
2024-08-07 15:32:32 +0900

    로그인 버튼 구현

--- src/@features/Auth/components/LoginForm.tsx
+++ src/@features/Auth/components/LoginForm.tsx
@@ -2,8 +2,8 @@ import { signIn } from '@/@features/Auth/api';
 import { useAuthModal } from '@/@features/Auth/components/AuthModal/useAuthModal';
 import { useAuth } from '@/@features/Auth/useAuth';
 import { LoginSchema, loginSchema } from '@/@features/Auth/yup';
-import GoogleButton from '@/components/atoms/GoogleButton';
-import KakaoButton from '@/components/atoms/KakaoButton';
+import GoogleButton from '@/@features/Auth/components/GoogleButton';
+import KakaoButton from '@/@features/Auth/components/KakaoButton';
 import LineText from '@/components/atoms/LineText';
 import * as S from '@/styles/index.style';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -29,28 +29,29 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(handleLogin)}>
-      <S.div.Column $gap={10} style={{ minWidth: '400px' }}>
-        <S.div.Row $justify="center">
-          <S.h.H1>로그인</S.h.H1>
-        </S.div.Row>
+    <S.div.Column $gap={10}>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <S.div.Column $gap={10} style={{ minWidth: '400px' }}>
+          <S.div.Row $justify="center">
+            <S.h.H1>로그인</S.h.H1>
+          </S.div.Row>
 
-        <S.input.Input type="email" placeholder="이메일" $size="medium" {...register('email')} />
-        {errors.email && <S.span.ErrorSpan>{errors.email.message}</S.span.ErrorSpan>}
+          <S.input.Input type="email" placeholder="이메일" $size="medium" {...register('email')} />
+          {errors.email && <S.span.ErrorSpan>{errors.email.message}</S.span.ErrorSpan>}
 
-        <S.input.Input type="password" placeholder="비밀번호" $size="medium" {...register('password')} />
-        {errors.password && <S.span.ErrorSpan>{errors.password.message}</S.span.ErrorSpan>}
+          <S.input.Input type="password" placeholder="비밀번호" $size="medium" {...register('password')} />
+          {errors.password && <S.span.ErrorSpan>{errors.password.message}</S.span.ErrorSpan>}
 
-        <S.button.Button $colors="primary" $height={50} type="submit">
-          <S.h.H5>이메일로 로그인</S.h.H5>
-        </S.button.Button>
+          <S.button.Button $colors="primary" $height={50} type="submit">
+            <S.h.H5>이메일로 로그인</S.h.H5>
+          </S.button.Button>
 
-        <LineText text="또는" />
-
-        <GoogleButton text="구글로 로그인하기" />
-        <KakaoButton text="카카오로 로그인하기" />
-      </S.div.Column>
-    </form>
+          <LineText text="또는" />
+        </S.div.Column>
+      </form>
+      <GoogleButton text="구글로 로그인하기" />
+      <KakaoButton text="카카오로 로그인하기" />
+    </S.div.Column>
   );
 };
 
